da40e1dae2df3b54a498bf5b83bdcf9bdf35274b
RedEyesC <1875077877@qq.com>
2024-09-04 11:36:41 +0800

    补充代码

--- Core/DataType.py
+++ Core/DataType.py
@@ -8,7 +8,7 @@ def TryParseValue(value, type):
     elif type == "arrayStr":
         return TryParseToArrayString(value)
     else:
-        print("识别到未定义类型" + type)
+        print("识别到未定义数据类型" + type)
         exit(-1)
 
 
@@ -48,3 +48,47 @@ def TryParseToArrayString(value: str):
         arrayString.append(TryParseToString(str))
 
     return arrayString
+
+
+def TryParseMode(model, fields, index):
+    if model == "map":
+        return TryParseToMap(fields, index)
+    elif model == "list":
+        return TryParseToList(fields, index)
+    else:
+        print("识别到未定义格式类型" + model)
+        exit(-1)
+
+
+def TryParseToMap(fields, index):
+    data = {}
+    for key in fields:
+        data[key] = GetField(fields, key)
+
+    return data
+
+
+def TryParseToList(fields, index):
+    keys = index.split(",")
+
+    data = CreateNestedDict(keys, fields)
+
+    return data
+
+
+def CreateNestedDict(keys, fields):
+    if not keys:
+        data = {}
+        for name in fields:
+            data[name] = GetField(fields, name)
+
+        return data
+
+    return {GetField(fields, keys[0]): CreateNestedDict(keys[1:], fields)}
+
+
+def GetField(fields, fieldName: str):
+    if fieldName in fields:
+        return fields[fieldName]
+    else:
+        return None
