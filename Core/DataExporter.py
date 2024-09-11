a7b16de83536a7f93082b211a0b1083caf8fb90c
RedEyesC <1875077877@qq.com>
2024-09-11 17:56:33 +0800

    支持导出bin文件

--- Core/DataExporter.py
+++ Core/DataExporter.py
@@ -31,33 +31,78 @@ def DataMerge(data, x: dict):
     return x
 
 
-class BinData:
-    type: int
-    value: any
-
-    def __init__(self, type, value):
-        self.type = type
-        self.value = value
-
-
 def ExportBin(table, records):
-    # 插入版本标志
-    byteArray: bytes = "DE1.0".encode("utf-8")
+    offset: int = 0
+
+    # 插入版本标志(以00为结尾)
+    byteArray: bytearray = bytearray("DE1.0", "utf-8")
     byteArray += b"\x00"
+    offset = offset + len(byteArray)
 
-    # 存放实际含有的数据
-    binMap: dict[str, int] = {}
-    binList: list[BinData] = []
-
-    # 统计需要存放的数据
+    # 合并好数据
+    x = {}
     for record in records:
-        data = record.data
+        DataMerge(record.data, x)
 
-    # 生成2进制表头，用于记录索引对应的偏移
+    binMap: dict = {}
+    # 生成入口dict
 
-    # 生成保存的数组
+    data2Bin(x, binMap, byteArray)
 
     # 生成文件
     with open(table["output"] + ".bin", "wb") as file:
         file.write(byteArray)
         file.close()
+
+
+def data2Bin(data, binMap, byteArray: bytearray):
+    offset = len(byteArray)
+
+    if isinstance(data, dict):
+        # 2进制字典结构由1字节的类型定义,2字节的长度定义,字典长度x(4字节键名索引+4字节键值索引) 组成
+        binData = b"\x01" + len(data).to_bytes(2, byteorder="big", signed=True)
+        binLen = 3 + len(data) * 8
+
+        # 预填充
+        byteArray[offset:offset] = bytearray(binLen)
+
+        for key in data:
+            binData = binData + data2Bin(key, binMap, byteArray)
+            binData = binData + data2Bin(data[key], binMap, byteArray)
+
+        # 删除预填充
+        del byteArray[offset : offset + binLen]
+
+    if isinstance(data, list):
+        # 2进制列表结构由1字节的类型定义,2字节的长度定义,列表长度x4字节值索引 组成
+        binData = b"\x02" + len(data).to_bytes(2, byteorder="big", signed=True)
+        binLen = 3 + len(data) * 4
+
+        # 预填充
+        byteArray[offset:offset] = bytearray(binLen)
+
+        offset = offset + binLen
+        for value in data:
+            binData = binData + data2Bin(value, binMap, byteArray)
+
+        # 删除预填充
+        del byteArray[offset : offset + binLen]
+
+    elif isinstance(data, int):
+        # 2进制int结构由1字节的类型定义,8字节的值定义
+        binData = b"\x03" + data.to_bytes(8, byteorder="big", signed=True)
+
+    elif isinstance(data, str):
+        # 2进制str结构由1字节的类型定义,2字节的长度定义,utf8编码的字符串长度组成
+        strData = data.encode("utf-8")
+        binData = b"\x04" + len(strData).to_bytes(2, byteorder="big", signed=False)
+        binData = binData + strData
+
+    if binData in binMap:
+        return binMap[binData]
+    else:
+        byteArray[offset:offset] = bytearray(binData)
+        binOffset = offset.to_bytes(4, byteorder="big", signed=False)
+        binMap[binData] = binOffset
+
+        return binOffset
